45e89469a14b0e65f7254ee2ef2738d6c24174c6
CreativeGP <social@cretgp.com>
2019-08-21 22:54:31 +0900

    :rotating_light: 詳細テストでパフォを計測するようにした

--- utest.sh
+++ utest.sh
@@ -1,8 +1,17 @@
-#!/bin/sh
+#!/bin/zsh
+
+TIMEFMT='%J   %U  user %S system %P cpu %*E total'$'\n'\
+'avg shared (code):         %X KB'$'\n'\
+'avg unshared (data/stack): %D KB'$'\n'\
+'total (sum):               %K KB'$'\n'\
+'max memory:                %M MB'$'\n'\
+'page faults from disk:     %F'$'\n'\
+'other page faults:         %R'
+
 
 line_count=`cat testcases.em | wc -l`
 
-if [ "$1" = "verbose" ] ; then
+if [ "$1" = "v" ] ; then
     echo "wwwwwww TESTING wwwwwwww"
 fi
 
@@ -19,8 +28,7 @@ do
     ans=`sed -n ${lineI3}p testcases.em`
     blank=`sed -n ${lineI4}p testcases.em`
 
-    
-    if [ "$1" = "verbose" ] ; then
+    if [ "$1" = "v" ] ; then
         echo $title
         echo $code
     else
@@ -30,6 +38,11 @@ do
     res=`./emelio "$code"`
     if [ "$res" = "$ans" ] ; then
         echo -e "\e[1;32mPassed.\e[0m"
+
+        if [ "$1" = "v" ] ; then
+            time ./emelio "$code"
+            echo ""
+        fi
     else
         echo -e "\e[1;31mError!\e[0m "
         echo "Expected '$ans', but '$res'.";
